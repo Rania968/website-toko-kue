f22c63cc5811efb468c6d5068c090c48253b2f7f
rani <541231181@student.smktelkom-pwt.sch.id>
2024-09-02 09:02:16 +0700

    541231181@student.smktelkom-pwt.sch.id

---
+++ tokokue/asset/script.js
@@ -0,0 +1,17 @@
+document.getElementById('loginForm').addEventListener('submit', function(event){
+    event.preventDefault();
+    
+    const correctUsername = 'imut';
+    const correctPassword = 'imut';
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    
+    if(username === correctUsername && password === correctPassword){
+        alert("login berhasil");
+        window.location.href = 'index.html';
+    }else{
+        alert("username atau password tidak sesuai");
+    }
+    
+    });
